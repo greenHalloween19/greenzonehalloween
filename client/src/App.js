2180ff154303e9b3e0e6f7875eef5ffce700891a
Shane4666 <sc897@nau.edu>
2019-10-16 20:51:20 -0700

    linter fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.scss';
 
-const App = () => <h1>🎉 The Green Zone is the best zone. 🎉</h1>;
+const App = () => (
+  <h1>
+    <span role="img" aria-label="hype">
+      🎉
+    </span>{' '}
+    The Green Zone is the best zone.{' '}
+    <span role="img" aria-label="hype">
+      🎉
+    </span>
+  </h1>
+);
 
 export default App;
