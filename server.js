9784bf79991475b7c262a77923f491c8be74ab84
Shane4666 <sc897@nau.edu>
2019-10-16 20:09:50 -0700

    Scores API Finished (minus security stuff and delete by id)

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 // Call the connect to DB function.
 connectToDB();
 
+// Middleware initialization
+app.use(express.json({ extended: false }));
+
 // API Routes
 app.get('/', (req, res) => res.send('Green Zone Halloween API.'));
 app.use('/scores', require('./routes/api/scores'));
-
-
 
 // Looks for env variable (need for Heroku), if it doesnt exist we will just use port 5000.
 const PORT = process.env.PORT || 5000;
