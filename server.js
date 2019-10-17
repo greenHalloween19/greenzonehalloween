60fd4d3d9991c7013abe144a0a0b294d9158f6b3
Shane4666 <sc897@nau.edu>
2019-10-16 19:02:21 -0700

    DB Connection.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectToDB = require('./config/db');
 
 const app = express();
+
+// Call the connect to DB function.
+connectToDB();
+
 app.get('/', (req, res) => res.send('API WILL BE HERE SOON!'));
 
 // Looks for env variable (need for Heroku), if it doesnt exist we will just use port 5000.
