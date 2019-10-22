42d7dcb2947e0036621d7b15c5db3aef786da910
Shane4666 <sc897@nau.edu>
2019-10-22 14:41:12 -0700

    game update.

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,28 +1,44 @@
 import React, { useState } from 'react';
 import SignupForm from './SignupForm';
 import OogieBoogie from '../assets/OogieBoogie.png';
+import { tiles } from '../data/tiles';
 
 const GamePage = () => {
   const [gameState, setGameState] = useState(1);
   const [currentUser, setCurrentUser] = useState('');
   const [currentTileNumber, setCurrentTileNumber] = useState(0);
-  const [isRolling, setRolling] = useState(false);
+  const [currentTileData, setCurrentTileData] = useState(null);
+  const [lastSpinnedNumber, setSpinnedNumber] = useState(0);
+  const [isSpinning, setSpinning] = useState(false);
+  const [isConfirmingSpin, setConfirmingSpin] = useState(false);
+  const [ currentPoints, setCurrentPoints ] = useState(0);
 
   const userInfoSubmitted = username => {
     setGameState(2);
     setCurrentUser(username);
   };
 
-  const rollTheDice = () => {
-    setRolling(true);
+  const spinTheSpinner = () => {
+    setSpinnedNumber(0);
+    setSpinning(true);
+    // Random # 1 - 10.
+    const spinnedNumber = Math.floor(Math.random() * 10) + 1;
+    const tileIndex = Math.floor(
+      Math.random() * (tiles['example-area-options'].length - 1)
+    );
     setTimeout(() => {
-      setRolling(false);
+      setSpinnedNumber(spinnedNumber);
+      setCurrentTileData(tiles['example-area-options'][tileIndex]);
+      setCurrentTileNumber(currentTileNumber + spinnedNumber);
+      setCurrentPoints(currentPoints + (tiles['example-area-options'][tileIndex].points));
+      setSpinning(false);
+      setConfirmingSpin(true);
     }, 2000);
   };
 
   const startGame = () => {
     setGameState(3);
-    rollTheDice();
+    spinTheSpinner();
   };
 
   return (
@@ -39,25 +55,46 @@ const GamePage = () => {
         <section className="game__section">
           <h1 className="game__header">{currentUser}</h1>
           <h2>Welcome to the game!</h2>
-          <p>Please roll the dice below when you are ready to continue!</p>
-          <button
+          <p>
+            Please spin using the button below when you are ready to continue!
+          </p>
+          <input
             onClick={() => startGame()}
             type="button"
-            class="roll-button button-control"
-          >
-            Roll The Dice!{' '}
-            <span class="button-emoji" role="img" aria-label="roll the dice">
-              🎲
-            </span>
-          </button>
+            className="roll-button button-control"
+            value="Spin!"
+          ></input>
         </section>
       )}
-      {gameState === 3 && (
-        <section className="game__section">
-          <h1>You are on tile number: {currentTileNumber}</h1>
+      {gameState === 3 && currentTileData && (
+        <section className="game__section game__section--playing">
+          <div>
+            <h1 className="primary-title">{currentUser}</h1>
+            <h3>
+              Go to tile:{' '}
+              <span className="primary-title">{currentTileNumber}</span>
+            </h3>
+            <h3>
+              Current Score: <span className="primary-title">{currentPoints}</span>
+            </h3>
+          </div>
+          <section className="game__outcome">
+            <h3 className="primary-title">{currentTileData.name}</h3>
+            <p>{currentTileData.desc}</p>
+            <div className={currentTileData.points > 0 ? 'gain' : 'lose'}>
+              <p>You {currentTileData.points > 0 ? 'gained' : 'lost'}:</p>
+              <p> {currentTileData.points} pts!</p>
+            </div>
+          </section>
+          <input
+            onClick={() => spinTheSpinner()}
+            type="button"
+            className="roll-button button-control"
+            value="Spin Again"
+          ></input>
         </section>
       )}
-      {isRolling && (
+      {isSpinning && (
         <div className="rolling-overlay">
           <div>
             <img
@@ -66,7 +103,22 @@ const GamePage = () => {
               alt="Oogie Boogie"
             ></img>
           </div>
-          <h1>Rolling...</h1>
+          <h1>Spinning...</h1>
+        </div>
+      )}
+
+      {isConfirmingSpin && (
+        <div className="rolling-overlay">
+          <h1 className="primary-title">You've spun the number:</h1>
+          <h1>{lastSpinnedNumber}</h1>
+          <div>
+            <input
+              onClick={() => setConfirmingSpin(false)}
+              type="button"
+              className="roll-button button-control"
+              value="Confirm"
+            ></input>
+          </div>
         </div>
       )}
     </div>
