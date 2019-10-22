574dd0660ec085d7710be8e7172786e4fe5e36ad
Shane4666 <sc897@nau.edu>
2019-10-21 23:52:56 -0700

    basic form setup.

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GamePage = () => {
-return (
-  <div>Game Page!</div>
-)
+  const [gameState, setGameState] = useState(1);
+  const [userName, setUsername] = useState('');
+  const [submissionError, setSubmissionError] = useState('');
 
+  const onSubmit = e => {
+    e.preventDefault();
+    setSubmissionError('');
+    if (userName.length < 10 && userName.length > 1) {
+      setGameState(2);
+      return;
+    }
+    setSubmissionError('A Username between the length of 1 - 9 characters is required.');
+  };
+
+  return (
+    <div className="game">
+      {gameState === 1 && (
+        <div className="game-form">
+          <form onSubmit={e => onSubmit(e)}>
+            <input
+              type="text"
+              onChange={e => setUsername(e.target.value)}
+              value={userName}
+              aria-label="Enter Username"
+              placeholder="Enter a username"
+            ></input>
+            <input type="submit" value="Submit"></input>
+            {submissionError && <label className="error-label">{submissionError}</label>}
+          </form>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GamePage;
