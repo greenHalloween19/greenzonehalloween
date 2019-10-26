a7bf493710607f9a20ec78b126651603a611e062
Shane4666 <sc897@nau.edu>
2019-10-25 22:39:44 -0700

    path choice logic added. (janky tho, need to refactor it later)

--- client/src/components/WelcomeSection.js
+++ client/src/components/WelcomeSection.js
@@ -5,12 +5,28 @@ const WelcomeSection = ({ currentUser, onSpin }) => (
     <h1 className="game__header">{currentUser}</h1>
     <h2>Welcome to the game!</h2>
     <p>Please spin using the button below when you are ready to continue!</p>
-    <input
-      onClick={() => onSpin()}
-      type="button"
-      className="roll-button button-control"
-      value="Spin!"
-    ></input>
+    <div className="path-choices">
+      <label htmlFor="TheVoid">
+        <input
+          id="TheVoid"
+          onClick={() => onSpin(true)}
+          type="button"
+          className="roll-button button-control"
+          value="Spin!"
+        ></input>
+        The Void
+      </label>
+      <label htmlFor="graveyard">
+        <input
+          id="graveyard"
+          onClick={() => onSpin(false)}
+          type="button"
+          className="roll-button button-control"
+          value="Spin!"
+        ></input>
+        Graveyard
+      </label>
+    </div>
   </section>
 );
 export default WelcomeSection;
