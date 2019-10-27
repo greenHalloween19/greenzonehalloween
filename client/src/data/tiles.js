28a380de598750c84a56967dc81095b5f4dd0cac
Shane4666 <sc897@nau.edu>
2019-10-26 20:12:48 -0700

    tiles data updated.

--- client/src/data/tiles.js
+++ client/src/data/tiles.js
@@ -1,57 +1,721 @@
+import Jack from '../assets/Jack.svg';
+
 export const tiles = {
-  'example-area-options': [
+  Graveyard: [
     {
-      name: 'Candy Land',
-      desc: 'You eat some yummy candy!',
-      points: 2500
+      icon: Jack,
+      desc:
+        'There are few who can deny at what you do you are the best. For your talents are renowned far and wide',
+      points: 100
     },
     {
-      name: "Jack's House",
-      desc: 'Jack feeds you a hearty meal!',
-      points: 3000
+      icon: Jack,
+      desc:
+        'With the wave of your hand and a well-placed moan you have swept the very bravest off their feet',
+      points: 110
     },
     {
-      name: "Santa's Sleigh",
-      desc: "You find Santa's sleigh!",
-      points: 4000
+      icon: Jack,
+      desc:
+        'Gaze at the moon and stars with Zero on the mountain',
+      points: 150
     },
     {
-      name: 'Corn Chip Valley',
-      desc: 'You stub your toe on a chip and trip.',
+      icon: Jack,
+      desc:
+        'No animal of man can scream like you can',
+      points: 75
+    },
+    {
+      icon: Jack,
+      desc:
+        'When it comes to surprises in the moonlit night, you excel without ever even trying',
+      points: 115
+    },
+    {
+      icon: Jack,
+      desc:
+        'Played fetch with Zero',
+      points: 175
+    },
+    {
+      icon: Jack,
+      desc:
+        'You can\'t wait until next Halloween cause you\'ve got some new ideas that will really make them scream',
+      points: 145
+    },
+    {
+      icon: Jack,
+      desc:
+        'Enjoy a nice walk with Zero through the graveyard',
+      points: 160
+    },
+    {
+      icon: Jack,
+      desc:
+        'Zero enjoys hearing the stories you share with him',
+      points: 125
+    },
+    {
+      icon: Jack,
+      desc:
+        'You enjoy a relaxing moment under the stars',
+      points: 175
+    },
+    {
+      icon: Jack,
+      desc:
+        'With the slightest little effort of your ghostlike charms, you have seen grown adults give out a shriek',
+      points: 90
+    },
+    {
+      icon: Jack,
+      desc:
+        'You found Zero at his house',
+      points: 85
+    },
+    {
+      icon: Jack,
+      desc:
+        'Oh no someone has followed you to your quiet place at the graveyard',
+      points: -100
+    },
+    {
+      icon: Jack,
+      desc:
+        'Boo! You were spooked by a ghost',
+      points: -75
+    },
+    {
+      icon: Jack,
+      desc:
+        'You tripped over a gravestone. Ouch!',
+      points: -45
+    },
+    {
+      icon: Jack,
+      desc:
+        'Year after year, it’s the same routine. You\'ve grown so weary of the sound of screams.',
+      points: -90
+    }
+  ],
+  'Ring of Holiday Trees': [
+    {
+      icon: Jack,
+      desc:
+        'Discovered the Valentine\'s Tree',
+      points: 214
+    },
+    {
+      icon: Jack,
+      desc:
+        'Discovered the Easter Egg Tree',
+      points: 412
+    },
+    {
+      icon: Jack,
+      desc:
+        'Discovered the Shamrock Tree',
+      points: 317
+    },
+    {
+      icon: Jack,
+      desc:
+        'Discovered the Turkey Tree',
+      points: 1128
+    },
+    {
+      icon: Jack,
+      desc:
+        'Discovered the Christmas Tree, Tree',
+      points: 1224
+    },
+    {
+      icon: Jack,
+      desc:
+        'Discovered the holiday worlds of old',
+      points: 500
+    }
+  ],
+  'Vortex to Christmas Land': [
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 50
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 25
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 75
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 100
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 5
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 45
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 65
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 85
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 90
+    },
+    {
+      icon: Jack,
+      desc:
+        'Traveling to Christmas Town',
+      points: 10
+    }
+  ],
+  'Christmas Town': [
+    {
+      icon: Jack,
+      desc:
+        'You tasted the white things in the air!',
+      points: 200
+    },
+    {
+      icon: Jack,
+      desc:
+        'You covered trees with tiny little things and electric lights on strings',
+      points: 250
+    },
+    {
+      icon: Jack,
+      desc:
+        'You heard music in the air instead of screams',
+      points: 215
+    },
+    {
+      icon: Jack,
+      desc:
+        'You smelled the scent of cakes and pies absolutely everywhere',
+      points: 210
+    },
+    {
+      icon: Jack,
+      desc:
+        'You discovered Christmas town!',
+      points: 350
+    },
+    {
+      icon: Jack,
+      desc:
+        'You heard laughter instead of screaming',
+      points: 245
+    },
+    {
+      icon: Jack,
+      desc:
+        'You gathered round the fire to hear a jolly story',
+      points: 220
+    },
+    {
+      icon: Jack,
+      desc:
+        'There\'s frost on every window, you can\'t believe your eyes! And in your bones you feel the warmth that\’s coming from inside',
+      points: 215
+    },
+    {
+      icon: Jack,
+      desc:
+        'You were hit by a child throwing a snowball',
+      points: -200
+    }
+  ],
+  'Christmas Preparation Zone': [
+    {
+      icon: Jack,
+      desc:
+        'Making Christmas, making Christmas is so fine! You got the town to cheer so it\'s party time!',
+      points: 400
+    },
+    {
+      icon: Jack,
+      desc:
+        'Wrapped the most wonderful present',
+      points: 515
+    },
+    {
+      icon: Jack,
+      desc:
+        'Wrapped a box with bright-colored paper topped with a bow',
+      points: 415
+    },
+    {
+      icon: Jack,
+      desc:
+        'Filled an oversized sock not with a foot but with candy and small toys',
+      points: 475
+    },
+    {
+      icon: Jack,
+      desc:
+        'The Mayor thinks Christmas is a splendid idea and sounds like fun. He endorses the idea and says to try it at once',
+      points: 600
+    },
+    {
+      icon: Jack,
+      desc:
+        'Jack\'s hopes, dreams and fantasies are coming true!',
+      points: 550
+    },
+    {
+      icon: Jack,
+      desc:
+        'Surprised isn\'t he? You give Sandy Claws a vacation from Christmas this year',
+      points: 425
+    },
+    {
+      icon: Jack,
+      desc:
+        'Made a present that wasn\'t fresh or pleasant',
+      points: -350
+    },
+    {
+      icon: Jack,
+      desc:
+        'Transformed an old rat into a most delightful hat',
+      points: -400
+    },
+    {
+      icon: Jack,
+      desc:
+        'Snakes and mice get wrapped up so nice with spiders legs and pretty bows',
+      points: -425
+    },
+    {
+      icon: Jack,
+      desc:
+        'Sally had the most terrible vision about their Christmas plans',
+      points: -475
+    },
+    {
+      icon: Jack,
+      desc:
+        'That\'s not Sandy Claws! Oogie\'s boys kidnaped the Easter Bunny',
+      points: -575
+    },
+    {
+      icon: Jack,
+      desc:
+        'Something is missing from Jack\'s Sandy Claws suit.',
+      points: -375
+    }
+  ],
+  'Halloween Town': [
+    {
+      icon: Jack,
+      desc:
+        'Hail to the pumpkin song!',
+      points: 75
+    },
+    {
+      icon: Jack,
+      desc:
+        'Thanks to your brilliant leadership this was our most horrible Halloween yet!',
+      points: 350
+    },
+    {
+      icon: Jack,
+      desc:
+        'You are such scream, you are a witches fondest dream',
+      points: 300
+    },
+    {
+      icon: Jack,
+      desc:
+        'The town has gathered to celebrate your successful Halloween',
+      points: 250
+    },
+    {
+      icon: Jack,
+      desc:
+        'The town has agreed to your plan to take over Christmas this year',
+      points: 375
+    },
+    {
+      icon: Jack,
+      desc:
+        'Ride with the moon in the dead of night',
+      points: 315
+    },
+    {
+      icon: Jack,
+      desc:
+        'You won the frightening and honorable mention for your work this Halloween',
+      points: 390
+    },
+    {
+      icon: Jack,
+      desc:
+        'Town Meeting, Town Meeting… Jack has something to announce',
+      points: 300
+    },
+    {
+      icon: Jack,
+      desc:
+        'Your Xmas assignment is ready. The mayor sends you to the front of the line',
+      points: 385
+    },
+    {
+      icon: Jack,
+      desc:
+        'Zero\'s brilliant nose will light the way to Christmas',
+      points: 400
+    },
+    {
+      icon: Jack,
+      desc:
+        'Jack is back now, everyone sing. In our town of Halloween',
+      points: 800
+    },
+    {
+      icon: Jack,
+      desc:
+        'Ho Ho Ho… Happy Halloween. Sandy Claws brings snow to Halloween Town!',
+      points: 1000
+    },
+    {
+      icon: Jack,
+      desc:
+        'Scared by the one hiding under your bed with teeth ground sharp and eyes glowing red',
+      points: -200
+    },
+    {
+      icon: Jack,
+      desc:
+        'Spooked by the one hiding under your stairs with fingers like snakes and spiders in his hair',
+      points: -215
+    },
+    {
+      icon: Jack,
+      desc:
+        'Skeleton Jack caught you in the back and made you scream like a banshee',
+      points: -230
+    },
+    {
+      icon: Jack,
+      desc:
+        'The shadow on the moon at night filled your dreams to the brim with fright',
+      points: -350
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein says you aren\'t ready for so much excitement. You must leave the celebration early.',
+      points: -400
+    },
+    {
+      icon: Jack,
+      desc:
+        'The mayor needed your help planning next years Halloween but you weren\'t there',
+      points: -800
+    },
+    {
+      icon: Jack,
+      desc:
+        'The alarms have sounded, Jack has gone missing',
+      points: -850
+    },
+    {
+      icon: Jack,
+      desc:
+        'Ugh. Oogie\'s boys showed up to get a Christmas assignment',
       points: -500
     },
     {
-      name: "Boogie's Lair",
-      desc: 'You eat some yummy candy!',
+      icon: Jack,
+      desc:
+        'The fog\'s as thick as jelly brains. There goes Christmas.',
+      points: -1000
+    },
+    {
+      icon: Jack,
+      desc:
+        'Terrible news folks. The worst tragedy of our times. Jack as been blown to smithereens.',
       points: -1000
     }
   ],
-  'example-area-options2': [
+  'Jack\'s House': [
     {
-      name: 'Chop Shop',
-      desc: 'You eat a yummy sandwich!',
-      points: 2500
+      icon: Jack,
+      desc:
+        'What does it mean? These dolls and toys confuse you so, but confound it all you love it though.',
+      points: 500 
     },
     {
-      name: "Z's Dog House",
-      desc: 'Z eats your friend!',
-      points: -3000
+      icon: Jack,
+      desc:
+        'This Christmas thing is not as tricky as it seems. You hatch a plan to make Christmas belong to you',
+      points: 550 
     },
     {
-      name: "Santa's Shed",
-      desc: "You high five an elf!",
-      points: 4000
+      icon: Jack,
+      desc:
+        'Eureka! This year Christmas will be ours!',
+      points: 650 
     },
     {
-      name: 'Icicle Mishap',
-      desc: 'You slip and hurt your back',
-      points: -500
+      icon: Jack,
+      desc:
+        'You bet you can improve Christmas and that\'s exactly what you\'ll do.',
+      points: 525 
     },
     {
-      name: "Boogie's House",
-      desc: 'Boogie gives you a trick instead of a treat!',
-      points: -1000
+      icon: Jack,
+      desc:
+        'Something\'s up with Jack and you don\'t know if we are ever going to get him back',
+      points: -525 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You learned the Christmas carols all by heart that your skull is full and its tearing you apart',
+      points: -515 
     }
   ],
-  totalNumberOfTiles: 50
+  'Finkelstein\'s Castle': [
+    {
+      icon: Jack,
+      desc:
+        'You finally escaped from Dr. Finkelstein\'s castle',
+      points: 700 
+    },
+    {
+      icon: Jack,
+      desc:
+        'The Frog\'s Breath successfully overpowered the odor of the poison in the soup you made.',
+      points: 750 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Used a trick spoon to fool Dr. Finkelstein into thinking his soup was poison free',
+      points: 775 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein agrees to release you from his lair if you promise to behave',
+      points: 680
+    },
+    {
+      icon: Jack,
+      desc:
+        'Igor brings the plans',
+      points: 625 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein builds a new friend to have conversations with',
+      points: 715 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Sally finds her fog juice to stop Jack',
+      points: 645 
+    },
+    {
+      icon: Jack,
+      desc:
+        'There is something in the wind. Sally senses tragedy is at hand.',
+      points: 600 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Jack visits to borrow equipment for his experiment',
+      points: 675
+    },
+    {
+      icon: Jack,
+      desc:
+        'The deadly nightshade you tried to poison Dr. Finkelstein with wore off',
+      points: -800
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein is suspicious and wants you to try the poisoned soup before he eats it',
+      points: -700 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein says he has been poisoned for the last time. He locks you away in his castle.',
+      points: -750 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You accidentally spill the bottle of deadly nightshade',
+      points: -675 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Dr. Finkelstein catches you before you escape',
+      points: -725 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Oh no you ran out of Frog\'s Breath',
+      points: -615 
+    }
+  ],
+  'Oogie Boogie\'s Lair': [
+    {
+      icon: Jack,
+      desc:
+        'Mr. Oogie Boogie got his kicks when you kidnapped the Sandy Claws',
+      points: 800 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Oogie Boogie rolls snake eyes. Looks like you won the jackpot!',
+      points: 850 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You defeat Oogie Boogie and save Santa and Sally!',
+      points: 875 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Naughty children never get any presents. You disobeyed Jack and brought Sandy Claws to Oogie Boogie',
+      points: -800 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Mr. Oogie Boogie says there\'s trouble close at hand. You\'d better pay attention now cause he\'s the boogie man',
+      points: -815 
+    },
+    {
+      icon: Jack,
+      desc:
+        'It\'s hopeless, you\'re finished. You haven\'t got a prayer. Cause he\'s Mr. Oogie Boogie and you ain\'t going nowhere',
+      points: -845 
+    },
+    {
+      icon: Jack,
+      desc:
+        'What?!? You tried to make a dupe out of Oogie Boogie. He caught you trying to help Sandy Claws escape',
+      points: -875 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Ashes to ashes, and dust to dust. Oogie Boogie is feeling weak… with hunger.',
+      points: -825 
+    }
+  ],
+  'Creepy Christmas Village': [
+    {
+      icon: Jack,
+      desc:
+        'You\'re welcome one and all. You successfully deliver presents down the chimneys',
+      points: 900
+    },
+    {
+      icon: Jack,
+      desc:
+        'Ho Ho Ho.. Santa Claus comes to save the day',
+      points: 975 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You fill the stockings with candies and small toys',
+      points: 915 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You hand a present to a small child and put a smile on their face',
+      points: 925 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Your snake started eating a family’s Christmas tree',
+      points: -980
+    },
+    {
+      icon: Jack,
+      desc:
+        'There you go sonny. You deliver a shrunken head as a present to a child for Christmas',
+      points: -925 
+    },
+    {
+      icon: Jack,
+      desc:
+        'The police are called. Families are being attacked by your toys',
+      points: -950 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Newscasters are reporting from all over the globe that an imposter is shamelessly impersonating Santa Claus',
+      points: -900 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Military units are mobilizing to stop you from continuing your Christmas plans',
+      points: -915 
+    },
+    {
+      icon: Jack,
+      desc:
+        'You\'re sleigh gets hit as you try to deliver presents to little Harry and Jordan who are next on your list',
+      points: -975 
+    },
+    {
+      icon: Jack,
+      desc:
+        'Attention, attention citizens. Terrible news. There is still no sign of Santa Claus.',
+      points: -1000 
+    }
+  ],
+  totalNumberOfTiles: 112
 };
