0490b3cae5713724c335c344f90ebf14a695aa24
Shane4666 <sc897@nau.edu>
2019-10-27 21:45:00 -0700

    scores api update to add characterId

--- models/Score.js
+++ models/Score.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const ScoreSchema = new mongoose.Schema({
+  characterId: {
+    type: Number,
+    required: true
+  },
   name: {
     type: String,
     required: true
