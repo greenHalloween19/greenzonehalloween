7f863d05fa38e431242372ad008c8ec55b13a09f
Shane4666 <sc897@nau.edu>
2019-10-28 18:55:49 -0700

    character update.

--- client/src/data/characters.js
+++ client/src/data/characters.js
@@ -1,14 +1,11 @@
 import Jack from '../assets/Jack.svg';
 import OogieBoogie from '../assets/OogieBoogie.svg';
 import Sally from '../assets/Sally.svg';
+import Zero from '../assets/Zero.png';
 
 export const CHARACTER_LIST = [
   { id: 1, name: 'Jack', img: Jack },
   { id: 2, name: 'Sally', img: Sally },
   { id: 3, name: 'Oogie', img: OogieBoogie },
-  { id: 4, name: 'Oogie', img: OogieBoogie },
-  { id: 5, name: 'Oogie', img: OogieBoogie },
-  { id: 6, name: 'Oogie', img: OogieBoogie },
-  { id: 7, name: 'Oogie', img: OogieBoogie },
-  { id: 8, name: 'Oogie', img: OogieBoogie }
+  { id: 4, name: 'Zero', img: Zero }
 ];
